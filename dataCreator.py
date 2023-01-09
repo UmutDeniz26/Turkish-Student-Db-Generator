eb154d0aecd017655e13fe70b1757da418b16f2a
Umut <umutcandeniz2101@hotmail.com>
2023-01-09 12:12:33 +0300

    first commit

---
+++ dataCreator.py
@@ -0,0 +1,42 @@
+# encoding:utf-8
+
+import random
+def format2Digit(number):
+    if 0 < number < 10:
+        return "0" + str(number)
+    else:
+        return str(number)
+    
+pobs=["Eskisehir","Istanbul","Ankara","Bursa","Izmir"];
+
+depts=["Bilgisayar Müh.","Elektrik-Elektronik Müh.","Endüstri Müh.","İnşaat Müh."]
+
+f=open('data.json', 'w', encoding='utf-8')
+names=open('isimler.txt','r',encoding='utf-8')
+lines =names.readlines()
+
+lastnames=open('soyisimler.txt','r',encoding='utf-8')
+lastlines =lastnames.readlines()
+
+sehirler=open('sehirler.txt','r',encoding='utf-8')
+sehirlerLines =sehirler.readlines()
+
+
+f.write('{"students":[')    
+numberOfStudents=300;
+for i in range(numberOfStudents):
+    f.write('\n{')
+    f.write('\n\t"id": '+str(i+1)+',')
+    f.write('\n\t"fname": "'+lines[random.randint(1,len(lines)-1)].replace('\n','').title()+'"'+',')
+    f.write('\n\t"lname": "'+lastlines[random.randint(1,len(lastlines)-1)].replace('\n','').title()+'"'+',')
+    f.write('\n\t"num": "15212017'+format2Digit(random.randint(1,99))+""+format2Digit(random.randint(1,99))+'",')
+    f.write('\n\t"dept": "'+str(random.randint(1,4)) +'",')
+    f.write('\n\t"pob": "'+sehirlerLines[random.randint(0,len(sehirlerLines)-1)].replace('\n','').replace('\t','')+'",')
+    f.write('\n\t"dob": "'+str(random.randint(1997,2001))+'-'+str(format2Digit(random.randint(1,12)))+'-'+str(format2Digit(random.randint(1,30)))+'"')
+    f.write('\n}')
+    if i!=numberOfStudents-1:
+        f.write(',')
+    f.write('\n')
+f.write('\t]\n}')
+f.close()
+names.close()
---
+++ jsonToMySql.py
@@ -0,0 +1,23 @@
+import mysql.connector
+import json
+
+sql = "INSERT INTO students_table (fname, lname, num, dept, pob, dob) VALUES ('%s', '%s', %s, %s, '%s', '%s')"
+f=open('data.json','r',encoding='utf-8');
+
+jsonDatabase=json.load(f)
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="112233112233As.",
+  database="students"
+)
+
+for elements in jsonDatabase['students']:
+  val=(elements['fname'],elements['lname'],str(elements['num']),elements['dept'],elements['pob'],str(elements['dob']))
+  mycursor = mydb.cursor()
+  mycursor.execute(sql %val)
+  mydb.commit()
+
+
+print(mycursor.rowcount, "record inserted.")
